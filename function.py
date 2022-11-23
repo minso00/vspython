4b322eea22b9cf510a2de293f5ba81e3aa80527c
Minji Kim <promingi7651@naver.com>
2022-11-23 10:17:51 +0900

    function sub update

--- function.py
+++ function.py
@@ -1,4 +1,10 @@
 def adder(num1, num2):
-    return num1+num2
+    print(num1+num2)
+    return
 
-print(adder(123, 234))
+def sub(num1, num2):
+    print(num1-num2)
+    return 
+
+adder(123, 234)
+sub(465, 456)
